b4352bbce7e964a69ecbd026da3d744eb5f80e68
tracij1286 <tracij50@gmail.com>
2022-12-05 20:13:48 -0500

    added quiz questions and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,35 @@
 
-
-/** 
- * Criteria 
- */
-
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-
-/** 
- * DEFINE VARIABLES 
- */
-
- // Define a set of questions
- const questions = [
+// Defined questions for quiz
+ var questions = [
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
-        answer: "d. <script>"
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: ["a. set", "b. div", "c. Both A and B", "d. var"],
+        answer: "d. var"
     },
     {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
-        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
-        answer: "c. quotes"
+        question: "Which of the following function of String object causes a string to be displayed in the specified size as if it were in a <font size = 'size'> tag?",
+        choices: ["a. fixed()", "b. fontcolor()", "c. fontsize()", "d. bold()"],
+        answer: "c. fontsize()"
     },
     {
-        question: "Arrays in JavaScript can be used to store _____.",
-        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
-        answer: "b. other arrays"
+        question: "Javascript was designed for which of the following purposes?",
+        choices: ["a. to style HTML pages", "b. to perform server side scripting opertion", "c. to add interactivity to HTML pages", "d. all of the above"],
+        answer: "c. to add interactivity to HTML pages"
     },
     {
-        question: "Commonly used data types DO NOT include:",
-        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
-        answer: "c. alerts"
-    },
-    {
-        question: "How do you create a function in JavaScript",
-        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
-        answer: "b. function myFunction()"
-    },
-    {
-        question: "How do you call a function named myFunction?",
-        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
-        answer: "c. myFunctions()"
-    },
-    {
-        question: "To see if two variables are equal in an if / else statement you would use ____.",
-        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
-        answer: "b. =="
-    },
-    {
-        question: "The first index of an array is ____.",
-        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
-        answer: "a. 0"
-    },
-    {
-        question: "Who invented JavaScript?",
-        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
-        answer: "c. Brendan Eich"
+        question: "Arrays in JavaScript are defined by which of the following statements?",
+        choices: ["a. It is an ordered list of objects", "b. It is an ordered list of string", "c. It is an ordered list of values", "d. It is an ordered list of functions"],
+        answer: "c. It is an ordered list of values"
     },
     {
         question: "How to write an IF statement in JavaScript?",
-        choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
-        answer: "c. if(i == 5)"
+        choices: ["a. if(i == 5)", "b. if i = 5 then", "c. if i == 5 then", "d. if i = 5"],
+        answer: "a. if(i == 5)"
     },
     {
         question: "How do you add a comment in a JavaScript?",
         choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
         answer: "a. //This is a comment"
-    },
-    {
-        question: "Which event occurs when the user clicks on an HTML element?",
-        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
-        answer: "a. onclick"
     }
 ];
 
@@ -124,10 +73,10 @@ var questionIndex = 0;
  */
 
 // WHEN I click the start button, timer starts
-var totalTime = 151;
+var totalTime = 61;
 function newQuiz() {
     questionIndex = 0;
-    totalTime = 150;
+    totalTime = 60;
     timeLeft.textContent = totalTime;
     initialInput.textContent = "";
 
